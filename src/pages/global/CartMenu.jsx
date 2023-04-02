924f8562649b473d466b8b309e41caad719d0454
Smith Ringtho <sringtho@gmail.com>
2023-04-02 12:37:11 +0300

    Added the item component to handle a particular item

--- src/pages/global/CartMenu.jsx
+++ src/pages/global/CartMenu.jsx
@@ -2,7 +2,7 @@ import { useSelector, useDispatch } from "react-redux"
 import { Box, Button, Divider, IconButton, Typography } from "@mui/material"
 import CloseIcon from "@mui/icons-material/Close"
 import AddIcon from "@mui/icons-material/Add"
-import RemoveIcon from "@mui/icons-material/Add"
+import RemoveIcon from "@mui/icons-material/Remove"
 import styled from "@emotion/styled"
 import { shades } from "../../theme"
 
